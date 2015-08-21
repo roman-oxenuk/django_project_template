f8a78f3a9ee112f59e454c8cf269120514b1646b
Roman <roman.oxenuk@gmail.com>
2015-08-21 21:51:21 +0300

    add requirements.txt and fix accounts/views.py

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def login(request):
     if request.user.is_authenticated():
-        return redirect(reverse_lazy('myads_list'))
+        return redirect(reverse_lazy('hello'))
     else:
         form = AuthenticationForm(data=request.POST or None)
         if form.is_valid():
             login_function(request, form.get_user())
-            return redirect('myads_list')
+            return redirect(reverse_lazy('hello'))
         else:
             return render(request, 'accounts/login_form.html', {'form': form})
     return response
