dc7f2a963d4ac9dc4cde105d82ccd6247bf7c200
Roman <roman.oxenuk@gmail.com>
2015-10-01 09:52:53 +0300

    add ALLOWED_HOSTS = ['*'] in settings/common.py

--- project/settings/common.py
+++ project/settings/common.py
@@ -34,7 +34,7 @@ SECRET_KEY = '{{ secret_key }}'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
