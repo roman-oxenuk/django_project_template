8bb562d357792be8bb00e68348e7d88fd9194e3e
Roman <roman.oxenuk@gmail.com>
2015-10-01 12:17:51 +0300

    a beauty fixies

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.development")
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
 
     from django.core.management import execute_from_command_line
 
--- project/wsgi.py
+++ project/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.development")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
 
 application = get_wsgi_application()
